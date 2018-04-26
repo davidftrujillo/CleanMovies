667e0303b4e628b87bb8c73e0952c694802410e3
David <david.fernandez.trujillo@gmail.com>
2018-04-26 20:39:38 +0200

    Add serialized names for Movie model

--- data/src/main/java/com/dft3dev/data/model/Movie.kt
+++ data/src/main/java/com/dft3dev/data/model/Movie.kt
@@ -1,19 +1,21 @@
 package com.dft3dev.data.model
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by david on 26/4/18.
  */
 data class Movie(
-    val adult: Boolean,
-    val backdropPath: String,
-    val genres: List<Genre>,
-    val id: Int,
-    val originalLanguage: String,
-    val originalTitle: String,
-    val overview: String,
-    val posterPath: String,
-    val releaseDate: String,
-    val title: String,
-    val voteAverage: Float,
-    val voteCount: Int
+        val adult: Boolean,
+        @SerializedName("backdrop_path") val backdropPath: String,
+        val genres: List<Genre>,
+        val id: Int,
+        @SerializedName("original_language") val originalLanguage: String,
+        @SerializedName("original_title") val originalTitle: String,
+        val overview: String,
+        @SerializedName("poster_path") val posterPath: String,
+        @SerializedName("release_date") val releaseDate: String,
+        val title: String,
+        @SerializedName("vote_average") val voteAverage: Float,
+        @SerializedName("vote_count") val voteCount: Int
 )
