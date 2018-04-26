edeaa2bb969d0c036f39246ee3a76851043beb29
David <david.fernandez.trujillo@gmail.com>
2018-04-26 19:42:12 +0200

    Initial commit

---
+++ app/src/main/java/com/dft3dev/cleanmovies/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dft3dev.cleanmovies
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
