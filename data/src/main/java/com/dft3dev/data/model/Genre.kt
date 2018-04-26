c7b08d262a9f680358b0b3719e86942cc088ee98
David <david.fernandez.trujillo@gmail.com>
2018-04-26 20:31:30 +0200

    Add models for data layer: Movie and Genre

---
+++ data/src/main/java/com/dft3dev/data/model/Genre.kt
@@ -0,0 +1,9 @@
+package com.dft3dev.data.model
+
+/**
+ * Created by david on 26/4/18.
+ */
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ data/src/main/java/com/dft3dev/data/model/Movie.kt
@@ -0,0 +1,19 @@
+package com.dft3dev.data.model
+
+/**
+ * Created by david on 26/4/18.
+ */
+data class Movie(
+    val adult: Boolean,
+    val backdropPath: String,
+    val genres: List<Genre>,
+    val id: Int,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val overview: String,
+    val posterPath: String,
+    val releaseDate: String,
+    val title: String,
+    val voteAverage: Float,
+    val voteCount: Int
+)
