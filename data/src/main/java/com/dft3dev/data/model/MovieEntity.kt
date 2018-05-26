3fa08ef34f7af4a8bcb1890b6ef542e4c46539ad
David <david.fernandez.trujillo@gmail.com>
2018-05-26 13:39:45 +0200

    Define room entities for genre and movie

--- data/src/main/java/com/dft3dev/data/model/MovieEntity.kt
+++ data/src/main/java/com/dft3dev/data/model/MovieEntity.kt
@@ -1,15 +1,19 @@
 package com.dft3dev.data.model
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by david on 26/4/18.
  */
+@Entity(tableName = "Movie")
 data class MovieEntity(
+        @PrimaryKey
+        val id: Int,
         val adult: Boolean,
         @SerializedName("backdrop_path") val backdropPath: String,
         val genres: List<GenreEntity>?,
-        val id: Int,
         @SerializedName("original_language") val originalLanguage: String,
         @SerializedName("original_title") val originalTitle: String,
         val overview: String,
